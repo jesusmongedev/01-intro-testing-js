d437168fc6c4d028e1cabde2ffe9f3b38df159ec
Jesús Andrés Monge <jesus_jaml@outlook.com>
2023-10-21 10:07:56 -0500

    [FEAT] Initi monorepo first methods with tests using jest

---
+++ demos/src/02-math.js
@@ -0,0 +1,31 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) throw new Error('Cannot divide by zero ❌')
+  return a / b;
+}
+
+function average(array=[]) {
+  const length = array.length
+  if (!length) return null
+  if (array.some(item => typeof item !== 'number')) throw new Error('Items must be numbers')
+
+  const total = array.reduce((acc, val) => {
+    return acc += val
+  }, 0)
+  const result = total / length
+  return result
+}
+
+module.exports = {
+  sum,
+  multiply,
+  divide,
+  average
+}
