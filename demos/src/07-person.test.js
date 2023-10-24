d014bcc10ea194943e58b638937a90693ffd5bc7
Jesús Andrés Monge <jesus_jaml@outlook.com>
2023-10-24 14:54:38 -0500

    [ADD] 07 tests for class Person calculate IMC

---
+++ demos/src/07-person.test.js
@@ -0,0 +1,31 @@
+const Person = require('./07-person');
+
+describe('Test for Person', () => {
+  let person;
+  beforeAll(() => {
+    person = new Person('Jesus', -64.3, 1.69);
+  });
+
+  describe('calcIMC method', () => {
+    test('should return not found if no case is meeted', () => {
+      person.weight = -64.3;
+      const imc = person.calcIMC();
+      expect(imc).toBe('no found');
+    });
+    test('should return down IMC', () => {
+      person.weight = 44.3;
+      const imc = person.calcIMC();
+      expect(imc).toBe('down');
+    });
+    test('should return normal IMC', () => {
+      person.weight = 64.3;
+      const imc = person.calcIMC();
+      expect(imc).toBe('normal');
+    });
+    test('should return overweight IMC', () => {
+      person.weight = 74.3;
+      const imc = person.calcIMC();
+      expect(imc).toBe('overweight');
+    });
+  });
+});
