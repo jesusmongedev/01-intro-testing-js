63389d7b44ea276ea1085854145245a391db0e4a
Jesús Andrés Monge <jesus_jaml@outlook.com>
2023-11-01 20:33:39 -0500

    [ADD] Arrang Act Assert

--- demos/src/07-person.test.js
+++ demos/src/07-person.test.js
@@ -2,14 +2,18 @@ const Person = require('./07-person');
 
 describe('Test for Person', () => {
   let person;
+  // Arrange
   beforeAll(() => {
     person = new Person('Jesus', -64.3, 1.69);
   });
 
   describe('calcIMC method', () => {
     test('should return not found if no case is meeted', () => {
+      // Arrange
       person.weight = -64.3;
+      // Act
       const imc = person.calcIMC();
+      // Assert
       expect(imc).toBe('no found');
     });
     test('should return down IMC', () => {
